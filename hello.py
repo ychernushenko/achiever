4f51828b6c961282ef4215e5dd279acd9c9e5392
Aaron Gutierrez <amgutier@debian-vm.vm>
2013-09-14 16:50:24 -0400

    Facebook POST error fixed

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def hello():
     return render_template('index.html')
 
