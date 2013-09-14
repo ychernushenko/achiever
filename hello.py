51304d23f8d9b7ce18db214e5a0edb30fd40d110
Aaron Gutierrez <amgutier@debian-vm.vm>
2013-09-14 16:40:45 -0400

    bootstrap

--- hello.py
+++ hello.py
@@ -10,22 +10,3 @@ def hello():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-    
-
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-@app.route('/other')
-def other():
-    print 3
-    db = psycopg2.connect(host="ec2-54-225-68-241.compute-1.amazonaws.com",
-                         user="xazmiyanlmjvcz",
-                         passwd="7e7AgvwcRuoqWszdsTMxYb4edZ",
-                         db="ddg1e2pn2rcqt6")
-    print 4
-    cur = db.cursor()
-    cur.execute("SELECT * FROM test_table")
-    s = ""
-    for row in cur.fetchAll():
-        s = s + row[0] + ", " + row[1] + "<br/>"
-    return s
