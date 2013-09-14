d9dccfc4cffc3f410f82a949d78379f4dd5c5171
Aaron Gutierrez <amgutier@debian-vm.vm>
2013-09-14 17:20:29 -0400

    bootstrap update

--- hello.py
+++ hello.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route('/dashboard', methods=['POST', 'GET'])
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
