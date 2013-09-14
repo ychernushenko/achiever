3d18468f11278dcbd884502e18989c8485d60155
Aaron Gutierrez <amgutier@debian-vm.vm>
2013-09-14 18:54:49 -0400

    try 2

--- hello.py
+++ hello.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def hello():
-    return render_template('new.html')
-
-@app.route('/dashboard', methods=['POST', 'GET'])
-def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == "__main__":
