d70dc413cfee160801d09a346913072cf760034c
Aaron Gutierrez <amgutier@debian-vm.vm>
2013-09-14 16:21:40 -0400

    using bootstrap

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
