d3ff7f7259bf33a92583a5a8b65ed66fbaf6289f
Andrew Zeng <zeng.qed@gmail.com>
2013-09-14 21:14:14 -0400

    Hard Code

--- db_connect.py
+++ db_connect.py
@@ -9,27 +9,11 @@ app = Flask(__name__)
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-
-@app.route('/', methods=['POST', 'GET'])
-def getGoals():
-    print "Connecting to database ..."
-    conn = psycopg2.connect(database=url.path[1:], user=url.username,
-                            password=url.password,
-                            host=url.hostname,
-                            port=url.port)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM goals")
-    for record in cursor:
-        print record
-        print type(record)
-    return cursor
-
-
 def getGoalsForOwner(ownerId):
-    conn = psycopg2.connect(database=url.path[1:], user=url.username,
-                            password=url.password,
-                            host=url.hostname,
-                            port=url.port)
+    conn = psycopg2.connect(database="ddg1e2pn2rcqt6", user="xazmiyanlmjvcz",
+                            password="7e7AgvwcRuoqWszdsTMxYb4edZ",
+                            host="ec2-54-225-68-241.compute-1.amazonaws.com",
+                            port="5432")
     cursor = conn.execute("SELECT (name, details, status) from goals WHERE owner_id=%s", ownerId)
     return cursor
 
